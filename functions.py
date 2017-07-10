21b2ea6bb9e3ac89eaabd84dd688df51d355b9c2
Stivi7 <stivi.ndoni@gmail.com>
2017-07-10 16:43:56 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+
+def print_twice(bruce):        # print twice function.
+    print(bruce)        
+    print(bruce)
+
+
+
+
+
+def cat_twice(x, y):    #using a function inside a function with local variables.
+    cat = x + y
+    print(cat)
+
+
+x = 7
+y = 5
+
+cat_twice(x, y)
+
+
---
+++ funex1.py
@@ -0,0 +1,32 @@
+
+# ex no.1
+
+# def right_justify():                
+    #s = "monty"
+    #print(" " * 65, s)
+
+
+
+#right_justify()
+
+
+# ex no.2
+
+def do_twice(f, x):
+    f(x)
+    f(x)
+
+def print_twice(x):        # print twice function.
+    print(x)        
+    print(x)
+   
+
+def print_spam(x):
+    print('spam')
+
+def do_four(f, x):
+    do_twice(f, x)
+    do_twice(f, x)
+
+do_twice(print_twice, 'spam')
+do_four(print_twice, 'spam')
